13b96376a8f0e475f0b82aa5ca2a75f2b4b9c7bc
Caleb Wheeler <caleb.edward.wheeler@gmail.com>
2021-04-28 09:57:59 -0500

    feat: create a function that takes in the retrieved data and passes desired values into an HTML template that is then injected into my grid.

--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
 let searchInput;
+$('#pagination-cont').hide();
 
 const baseUrl = "https://scmq7n.a.searchspring.io/api/search/search.json";
 
@@ -16,6 +17,7 @@ const baseUrl = "https://scmq7n.a.searchspring.io/api/search/search.json";
         .then(data => { 
           console.log(data);
           
+          createItemTemplate( data );
           resolve( data );
         })
         .catch(error => {
@@ -25,7 +27,40 @@ const baseUrl = "https://scmq7n.a.searchspring.io/api/search/search.json";
     })
   };
 
-  
+  //Function that takes in the retrieved data and loops through and builds an HTML template for each result that is then injected into my grid element
+  const createItemTemplate = ( data ) => {
+    
+    let results = data.results;
+    let resultTemplate;
+
+      for(let result of results) {
+        if(result.on_sale[0] === "Yes" ) {
+          resultTemplate += `
+            <div class="grid-item">
+              <img class="item-img" src="${ result.imageUrl }" />
+              <div class="item-info">
+                <p class="item-name">${ result.name }</p>
+                <p><span class="item-og-price">$${( result.price * 1 ).toFixed(2)}</span>
+                <span class="current-price">$${( result.sale_price[0] * 1 ).toFixed(2)}</span>
+                </p>
+              </div>  
+            </div> 
+          `;
+        }
+        else {
+        resultTemplate += `
+          <div class="grid-item">
+            <img class="item-img" src="${ result.imageUrl }" />
+            <div class="item-info">
+              <p class="item-name">${ result.name }</p>
+              <p class="current-price">$${( result.price * 1 ).toFixed(2)}</p>
+            </div>  
+          </div>  
+        `;
+        }
+      }
+      $('#result-cont').html(resultTemplate);
+  }
 
 $(document).ready(() => {
 
